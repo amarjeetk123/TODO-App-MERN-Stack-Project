01924ef15e33728b58c1e3841b907fdac6de1e45
amarjeetk123 <ak123del@gmail.com>
2022-12-26 00:24:26 +0530

    again changing the folder structure

--- backend/App.js
+++ backend/App.js
@@ -4,17 +4,6 @@ const app = express()
 
 const userRoutes  = require("./routes/userRoutes")
 
-// to serve the frontend
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
-
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
