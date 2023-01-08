8ae9711b1c22ad199a25be1ec5d06e2e1556f6c0
amarjeetk123 <ak123del@gmail.com>
2023-01-08 17:00:23 +0530

    fixing the bud

--- backend/App.js
+++ backend/App.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 const express = require("express")
 const app = express()
-const cors = require("cors")
-
+var cors = require('cors')
 const userRoutes  = require("./routes/userRoutes")
 
 // middleware
