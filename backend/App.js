20a909ec11aef7013812d33d7e27271cb1854c30
amarjeetk123 <ak123del@gmail.com>
2023-01-08 16:49:08 +0530

    configure cors() as a middleware in backend

--- backend/App.js
+++ backend/App.js
@@ -1,13 +1,14 @@
 require("dotenv").config();
 const express = require("express")
 const app = express()
-
+const cors = require("cors")
 
 const userRoutes  = require("./routes/userRoutes")
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 const connectToDB = require("./config/database")
 connectToDB();
