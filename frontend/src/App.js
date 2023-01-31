9c451b7c9207d100c2217c9815b2fa54a8cc17f3
amarjeetk123 <ak123del@gmail.com>
2023-01-31 12:38:44 +0530

    doing removing two componenets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import Homepage from './component/Homepage';
-import {BrowserRouter , Route , Routes} from "react-router-dom"
-import {account} from "./appwrite/appwriteConfig"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { account } from "./appwrite/appwriteConfig"
 import Login from './component/Login';
 import Signup from './component/Signup';
 import { useEffect, useState } from 'react';
@@ -10,28 +10,28 @@ import { useEffect, useState } from 'react';
 export const BASE_URL = "https://todo-backend-2ony.onrender.com";
 
 function App() {
-  
+
   const [userDetails, setuserDetails] = useState()
-    useEffect(() => {
-      const getUserData = account.get();
-      getUserData.then(
-       function(response){
-         setuserDetails(response);
-       },
-       function(error){
-         console.log(error);
-       }
-      )
-     }, [])
+  useEffect(() => {
+    const getUserData = account.get();
+    getUserData.then(
+      function (response) {
+        setuserDetails(response);
+      },
+      function (error) {
+        console.log(error);
+      }
+    )
+  }, [])
 
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element = { <Homepage /> } />
-      <Route path='/login' element = { <Login /> } />
-      {/* <Route path='/home' element = { userDetails ? <Homepage /> : <Login /> } /> */}
-      <Route path='/register' element = {  <Signup />   } />
-    </Routes>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        {/* <Route path='/login' element={<Login />} /> */}
+        {/* <Route path='/home' element = { userDetails ? <Homepage /> : <Login /> } /> */}
+        {/* <Route path='/register' element={<Signup />} /> */}
+      </Routes>
     </BrowserRouter>
   );
 }
